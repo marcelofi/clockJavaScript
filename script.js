08976f0beb9c8d57428e83f7857dc7f44ff1af6c
marcelofi <marce.fernandezi@gmail.com>
2022-06-28 18:08:13 -0400

    the clock is running

---
+++ script.js
@@ -0,0 +1,19 @@
+setInterval(setClock,1000)
+
+const hourHand=document.querySelector('[data-hour-hand]')
+const minutesHand=document.querySelector('[data-minutes-hand]')
+const secondsHand=document.querySelector('[data-seconds-hand]')
+
+function setClock(){
+    const currentDAte=new Date()
+    const secondsRatio=currentDAte.getSeconds()/60
+    const minutesRatio=(secondsRatio + currentDAte.getMinutes())/60
+    const hoursRatio=(minutesRatio + currentDAte.getHours())/12
+    setRotation(secondsHand,secondsRatio)
+    setRotation(minutesHand,minutesRatio)
+    setRotation(hourHand,hoursRatio)
+}
+
+function setRotation(element, rotationRatio){
+    element.style.setProperty('--rotation',rotationRatio*360)
+}
